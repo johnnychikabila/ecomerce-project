af5f9ac2d1d6b7b953b53bd405f38b39b8f997be
ivaan <johnnychikabila@gmail.com>
2022-05-10 00:05:30 +0300

    refactor gh-pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (   <nav className='purple darken-4'>
         <div className="nav-wrapper">
-          <a href="/" className="brand-logo">Shop Project</a>
+          <a href="/ecomerce-project" className="brand-logo">Shop Project</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><a href="https://github.com/johnnychikabila/ecomerce-project" target= '_blank'>Git</a></li>
           </ul>
